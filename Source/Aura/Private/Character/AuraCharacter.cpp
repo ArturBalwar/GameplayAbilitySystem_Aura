9ebf94ded208ffbca3b37af2aaf8f4f323225c00
Artur Balwar <artineusz@gmail.com>
2025-01-03 18:38:27 +0100

    Player and Enemy Class

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright Artur Balwar
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,16 @@
+// Copyright Artur Balwar
+
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
