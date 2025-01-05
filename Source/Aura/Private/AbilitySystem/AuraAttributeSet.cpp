33c0ebd17089fac0d59c90eff5b1c81ea842a290
Artur Balwar <artineusz@gmail.com>
2025-01-05 18:32:18 +0100

    GAS Player State, Ability System, Attribute Set Classes

---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Artur Balwar
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Artur Balwar
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
