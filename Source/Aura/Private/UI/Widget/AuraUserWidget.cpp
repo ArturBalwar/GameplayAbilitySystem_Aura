39bcec7600c6852c28ecb5e54f3023e1d5f75236
Artur Balwar <artineusz@gmail.com>
2025-01-11 22:31:53 +0100

    User Widget and Widget Controller classes, Progress Globe

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Artur Balwar
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
